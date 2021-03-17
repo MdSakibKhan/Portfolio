8989511cac3b1a36683ba66682eac46b2dc0eeed
Your Name <hrk.sakib@gmail.com>
2021-03-18 05:18:52 +0600

    CV Updated

--- src/App.js
+++ src/App.js
@@ -2,36 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Portfolio from "./Components/Portfolio"
-import ClipLoader from "react-spinners/ClipLoader";
-import GridLoader from "react-spinners/ClipLoader";
 
-import {useState , useEffect} from "react"
+
 
 
 function App() {
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 2000);
-  }, []);
-
-
   return (
     <div className="App">
-    {loading?
-      <div style = {{height : "100vh", margin : "3in"}}>
-      <text className="display-2 text-light d-block">Welcome to My Portfolio</text>
-      <ClipLoader color={"#FFFFFF"} loading={loading} size={150} />
-      </div> 
-       :
-       <div>
        <Portfolio></Portfolio>
-       </div>
-      }
       </div>
   );
 }
